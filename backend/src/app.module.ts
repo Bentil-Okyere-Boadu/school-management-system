9dcc6a14bde4a7ad5502ea9198d048f481ce00a1
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2025-04-18 14:23:14 +0000

    feat(backend): initial user role and permission module setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,20 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+import { PermissionModule } from './permission/permission.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'school_management',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity.js'],
       synchronize: true,
-      logging: true,
+      //logging: true,
     }),
+    UserModule,
+    RoleModule,
+    PermissionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
