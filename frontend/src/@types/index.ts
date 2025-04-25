9a8c5493304bc6873e017166ebf5a09f23411735
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2025-04-25 04:24:16 +0000

    feat: add types and useSignUp hook

--- frontend/src/@types/index.ts
+++ frontend/src/@types/index.ts
@@ -1,4 +1,22 @@
-export interface AuthCredentials {
+export interface Role {
+    id: string;
+    name: string;
+}
+
+export enum ButtonType {
+    submit = "submit",
+    reset = "reset",
+    button = "button",
+  }
+
+export interface User {
+    id: string;
     email: string;
     password: string;
-  }
+    name: string;
+    role: Role;
+    status: string
+}
+
+export type AuthCredentials = Pick<User, "email"| "password">;
+export type SignUpPayload = Pick<User, "email" | "password" | "name" | "role" >;
--- frontend/src/hooks/auth.ts
+++ frontend/src/hooks/auth.ts
@@ -1,11 +1,19 @@
 import { useMutation } from "@tanstack/react-query"
 import { customAPI } from "../../config/setup"
-import { AuthCredentials } from "@/@types"
+import { AuthCredentials, SignUpPayload } from "@/@types"
 
 export const useLogin = (userDetails: AuthCredentials) => {
     return useMutation({ 
         mutationFn: () => {
             return customAPI.post(`/auth/login`, userDetails)
+        }
+    })
+}
+
+export const useSignUp = (signUpDetails: SignUpPayload) => {
+    return useMutation({ 
+        mutationFn: () => {
+            return customAPI.post(`/users`, signUpDetails)
         }
     })
 }
