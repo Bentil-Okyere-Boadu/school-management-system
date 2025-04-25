8c872ead41038d99ac91b6c8b9ba3df50f9e8a08
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2025-04-25 09:01:49 +0000

    feat: add loader to button

--- frontend/src/components/ActionButton.tsx
+++ frontend/src/components/ActionButton.tsx
@@ -1,17 +1,22 @@
 "use client";
 import React from "react";
+import Loader from "./Loader";
 
 interface ActionButtonProps {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
+  loading?: boolean;
 }
 
-const ActionButton: React.FC<ActionButtonProps> = ({ text, onClick }) => {
+const ActionButton: React.FC<ActionButtonProps> = ({ text, onClick, loading = false}) => {
   return (
     <button
       onClick={onClick}
-      className="w-full h-10 text-base font-semibold text-white bg-purple-500 rounded-md border-white border-solid cursor-pointer border-[0.3px]"
+      className={`w-full h-10 text-base font-semibold text-white bg-purple-500 rounded-md border-white border-solid cursor-pointer border-[0.3px] flex items-center justify-center ${
+        loading ? "opacity-65" : ""
+      }`}
     >
+      {loading? <Loader/> : ""}
       {text}
     </button>
   );
