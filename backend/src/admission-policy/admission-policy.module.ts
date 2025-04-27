9776e6c0c278fe2d1283da43cb0ade217faba601
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2025-04-27 13:13:41 +0000

    Add admission policy,grading system,fee structure module and service to be linked to school entity

---
+++ backend/src/admission-policy/admission-policy.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { AdmissionPolicyService } from './admission-policy.service';
+
+@Module({
+  providers: [AdmissionPolicyService]
+})
+export class AdmissionPolicyModule {}
---
+++ backend/src/fee-structure/fee-structure.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('fee-structure')
+export class FeeStructureController {}
