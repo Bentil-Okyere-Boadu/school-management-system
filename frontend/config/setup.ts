2feb58ee2e499814375bf1708ddce65cb819b194
Michael Acheampong <michaelacheampong32@yahoo.com>
2025-05-04 17:37:02 +0000

    fix: resolve authoken not being added on initial login

--- frontend/config/setup.ts
+++ frontend/config/setup.ts
@@ -2,15 +2,12 @@ import axios, { AxiosInstance } from "axios";
 import Cookies from "js-cookie";
 import config from './index';
 
-export const customAPI: AxiosInstance = axios.create({
+const customAPI: AxiosInstance = axios.create({
   baseURL: config().apiURL,
   withCredentials: false,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
-    authorization: Cookies.get("authToken")
-      ? `Bearer ${Cookies.get("authToken")}`
-      : null,
     "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
     "Content-Security-Policy": "default-src 'self'",
     "X-Content-Type-Options": "nosniff",
@@ -18,3 +15,15 @@ export const customAPI: AxiosInstance = axios.create({
     "Referrer-Policy": "no-referrer",
   },
 });
+
+customAPI.interceptors.request.use((config) => {
+  const token = Cookies.get("authToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization;
+  }
+  return config;
+});
+
+export { customAPI };
