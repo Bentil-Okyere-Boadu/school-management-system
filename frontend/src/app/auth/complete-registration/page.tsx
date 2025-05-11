3f366ca9cf0d7bced23cb5eda7297c03561afc07
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2025-05-11 12:49:03 +0000

    fix: wrap searchParams component with suspense

--- frontend/src/app/auth/complete-registration/page.tsx
+++ frontend/src/app/auth/complete-registration/page.tsx
@@ -2,16 +2,18 @@
 import AuthBg from '@/components/auth/AuthBg';
 import CompleteRegistration from '@/components/auth/CompleteRegistration';
 import { useSearchParams } from 'next/navigation';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const CompleteRegistrationPage = () => {
     const searchParams = useSearchParams();
   const token = searchParams.get('token') || "";
 
   return (
-    <AuthBg>
-        <CompleteRegistration token={token as string}/>
-    </AuthBg>
+    <Suspense>
+      <AuthBg>
+          <CompleteRegistration token={token as string}/>
+      </AuthBg>
+    </Suspense>
   )
   
 }
