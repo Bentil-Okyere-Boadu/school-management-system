9317a8e84c28241e56c10ddbfb38ba3817501965
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2025-05-13 15:50:14 +0000

    feat: types

--- frontend/src/@types/index.ts
+++ frontend/src/@types/index.ts
@@ -1,48 +1,55 @@
 export enum ButtonType {
-    submit = "submit",
-    reset = "reset",
-    button = "button",
-  }
-
-  export type School = {
-    id: string;
-    email: string;
-    address: string;
-    phone: string;
-    name: string;
-  }
-export interface User {
-    id: string;
-    email: string;
-    password?: string;
-    name: string;
-    role: Role;
-    status: string;
-    school: School;
+  submit = "submit",
+  reset = "reset",
+  button = "button",
 }
 
-export type AuthCredentials = Pick<User, "email"| "password">;
-export type SignUpPayload = Pick<User, "email" | "password" | "name" | "role" >;
+export type School = {
+  id: string;
+  email: string;
+  address: string;
+  phone: string;
+  name: string;
+}
+export interface User {
+  id: string;
+  email: string;
+  password?: string;
+  name: string;
+  role: Role;
+  status: string;
+  school: School;
+}
+
+export type AuthCredentials = Pick<User, "email" | "password">;
+export type SignUpPayload = Pick<User, "email" | "password" | "name" | "role">;
 
 export enum Roles {
-    SCHOOL_ADMIN="school_admin",
-    STUDENT="student",
-    TEACHER="teacher",
-    SUPER_ADMIN="super_admin",
-    PARENT="parent"
+  SCHOOL_ADMIN = "school_admin",
+  STUDENT = "student",
+  TEACHER = "teacher",
+  SUPER_ADMIN = "super_admin",
+  PARENT = "parent"
 }
 
 export type Role = {
-    id: string;
-    name: keyof typeof Roles;
-  };
+  id: string;
+  name: keyof typeof Roles;
+};
 
 export type FeeStructure = {
-    id: string;
-    feeTitle: string,
-     feeType: string,
-     amount: number,
-     appliesTo: string,
-     dueDate: string,
-     classes: string[]
-    }
+  id: string;
+  feeTitle: string;
+  feeType: string;
+  amount: number;
+  appliesTo: string;
+  dueDate: string;
+  classes: string[];
+}
+
+export type Grade = {
+  grade: string;
+  minRange: number | null;
+  maxRange: number | null;
+  id: string;
+}
