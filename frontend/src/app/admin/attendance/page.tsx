c2c39a5e68338a72c7f8d3418b73a457995a2e38
Michael Acheampong <michaelacheampong32@yahoo.com>
2025-05-31 18:49:21 +0000

    update showing of design

--- frontend/src/app/admin/attendance/page.tsx
+++ frontend/src/app/admin/attendance/page.tsx
@@ -53,11 +53,13 @@ const Attendance = () => {
   return (
     <div className="pb-8">
       {!alreadyDone && <div>Attendance</div>}
-      {alreadyDone && (<section className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-6 px-0.5">
+      {alreadyDone && (
+        <>
+        <section className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-6 px-0.5">
           {stats.map((stat, index) => (
             <StatCard key={index} {...stat} />
           ))}
-        </section>)}
+        </section>
       
       <div>
         <TabBar 
@@ -78,6 +80,8 @@ const Attendance = () => {
           </div>
         )}
       </div>
+      </>
+      )}
       
     </div>
   );
