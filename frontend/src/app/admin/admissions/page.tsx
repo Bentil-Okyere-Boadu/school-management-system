73077f8b5a49dba4198b73208bcffb405b8c267d
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2025-06-11 04:31:38 +0000

    feat: copy admission page link

--- frontend/src/app/admin/admissions/page.tsx
+++ frontend/src/app/admin/admissions/page.tsx
@@ -2,7 +2,12 @@
 import { AdmissionsAnalyticsTabSection } from '@/components/admin/admissions/AdmissionsAnalyticsTabSection';
 import { AdmissionsListTabSection } from '@/components/admin/admissions/AdmissionsListTabSection';
 import TabBar from '@/components/common/TabBar';
+import { useGetMe } from '@/hooks/school-admin';
+import { Button } from '@mantine/core';
+import { IconCopy } from '@tabler/icons-react';
+import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 export interface MetricCardProps {
   value: string;
@@ -20,6 +25,9 @@ export type TabListItem = {
 };
 
 const Admissions = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const {} = usePathname();
+
   const defaultNavItems: TabListItem[] = [
     { tabLabel: "Admissions Analytics", tabKey: "admissions-analytics" },
     { tabLabel: "Admissions List", tabKey: "admissions-list" },
@@ -30,13 +38,42 @@ const Admissions = () => {
     setActiveTabKey(item.tabKey);
   };
 
+  const {me} = useGetMe();
+
+  const copyText = async (schoolId: string) => {
+    const baseUrl = typeof window !== 'undefined' ? window.location.origin : '';
+    const textToCopy = baseUrl + '/admission-forms/' + schoolId;
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setIsCopied(true);
+      setTimeout(() =>{
+        if(isCopied) {
+          toast.success('Admission link copied to clipboard.')
+        }
+
+        setIsCopied(false)
+      }, 1500); // Reset after 2 seconds
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  };
+
+
   return (
     <div>
-      <TabBar 
-        items={defaultNavItems} 
-        activeTabKey={activeTabKey} 
-        onItemClick={handleItemClick}
-      />
+      <div className='flex justify-between'>
+        <TabBar 
+          items={defaultNavItems} 
+          activeTabKey={activeTabKey} 
+          onItemClick={handleItemClick}
+        />
+
+        <Button 
+          leftSection={<IconCopy size={24}/>}
+          onClick={() => copyText(`${me.school.id}`)}
+          
+          >Copy Admissions link</Button> 
+      </div>
 
       {activeTabKey === "admissions-analytics" && (
         <div>
