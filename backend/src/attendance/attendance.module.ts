6da87b754182403dd5153b8968601c5778a584a9
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2025-07-12 20:26:04 +0000

    feat: exclude holidays and weekends from attendance records

--- backend/src/attendance/attendance.module.ts
+++ backend/src/attendance/attendance.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attendance } from './attendance.entity';
 import { ClassLevel } from 'src/class-level/class-level.entity';
 import { Student } from 'src/student/student.entity';
+import { Holiday } from 'src/academic-calendar/entitites/holiday.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance, ClassLevel, Student])],
+  imports: [TypeOrmModule.forFeature([Attendance, ClassLevel, Student, Holiday])],
   providers: [AttendanceService],
 })
 export class AttendanceModule {}
